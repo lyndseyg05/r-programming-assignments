fccc99d687c18cdd7d6025d233e81ed50b59ece2
lyndseyg05 <lyndseyg05@gmail.com>
2025-11-09 23:45:18 -0500

    Fix logical operator bug in tukey_multiple function

---
+++ Assignment 11 Debugging R.R
@@ -0,0 +1,22 @@
+tukey.outlier <- function(vec) {
+  q1 <- quantile(vec, 0.25)
+  q3 <- quantile(vec, 0.75)
+  iqr <- q3 - q1
+  lower <- q1 - 1.5 * iqr
+  upper <- q3 + 1.5 * iqr
+  vec < lower | vec > upper
+}
+corrected_tukey <- function(x) {
+  outliers <- array(TRUE, dim = dim(x))
+  for (j in seq_len(ncol(x))) {
+    outliers[, j] <- outliers[, j] & tukey.outlier(x[, j])
+  }
+  outlier.vec <- logical(nrow(x))
+  for (i in seq_len(nrow(x))) {
+    outlier.vec[i] <- all(outliers[i, ])
+  }
+  outlier.vec
+}
+
+corrected_tukey(test_mat)
+
